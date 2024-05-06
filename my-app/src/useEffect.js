f133b2ef3e91dd5d36e56cd0383c50ed67a7e116
VietDuong <duongquocviet@gmail.com>
2024-05-06 16:15:13 +0700

    Implement element paragraph

--- my-app/src/useEffect.js
+++ my-app/src/useEffect.js
@@ -1,34 +1,65 @@
-import { useState, useRef, useEffect } from 'react';
+// import { useState, useRef, useEffect } from 'react';
+// <childComponent name='jonll' age={12} />
+// function VideoPlayer({ src, isPlaying }) {
+//   const ref = useRef(null);
 
-function VideoPlayer({ src, isPlaying }) {
-  const ref = useRef(null);
+//   useEffect(() => {
+//     if (isPlaying) {
+//       console.log('Calling video.play()');
+//       ref.current.play();
+//     } else {
+//       console.log('Calling video.pause()');
+//       ref.current.pause();
+//     }
+//   });
 
+//   return <video ref={ref} src={src} loop playsInline />;
+// }
+
+// export default function App() {
+//   const [isPlaying, setIsPlaying] = useState(false);
+//   const [text, setText] = useState('');
+//   function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>;
+//   }
+//   return (
+//     <>
+//       <input value={text} onChange={e => setText(e.target.value)} />
+//       <button onClick={() => setIsPlaying(!isPlaying)}>
+//         {isPlaying ? 'Pause' : 'Play'}
+//       </button>
+//       <Welcome name="John" />
+//       <VideoPlayer
+//         isPlaying={isPlaying}
+//         src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+//       />
+//     </>
+//   );
+// }
+
+
+import React, { useEffect, useState } from 'react';
+
+function Example() {
+  
+  const [count, setCount] = useState(0);
+  
   useEffect(() => {
-    if (isPlaying) {
-      console.log('Calling video.play()');
-      ref.current.play();
-    } else {
-      console.log('Calling video.pause()');
-      ref.current.pause();
-    }
-  });
-
-  return <video ref={ref} src={src} loop playsInline />;
-}
-
-export default function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [text, setText] = useState('');
+    // Gọi sau mỗi lần component được render
+    document.title = `You clicked ${count} times`;
+  }, [count]); // useEffect chỉ được gọi lại nếu count thay đổi
+  console.log(1231);
   return (
-    <>
-      <input value={text} onChange={e => setText(e.target.value)} />
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? 'Pause' : 'Play'}
+    
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
       </button>
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-      />
-    </>
+    </div>
+    
   );
+  
 }
+
+export default Example;
